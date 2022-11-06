13a48ed6eb4d1288031a2787ab23408d9b9320ed
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-06 16:20:24 -0300

    Added Postgresql

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "alurareceita.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'alura_receita',
+        "USER": 'postgres',
+        "PASSWORD": 'x7y23h06',
+        "HOST": 'localhost'
+        
     }
 }
 
