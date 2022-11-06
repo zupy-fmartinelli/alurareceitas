acc81905d958580050ee7bb565068219a1ec1fa3
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-06 17:17:47 -0300

    Added Database templates

--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from .models import Receita #importa os modelos que estão nas classes
+
 #from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
     # return HttpResponse('<h1>Receitas</h1> <h2>Bem Vindo</h2>')
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de Legumes',
-        3:'Sorvete',
-        4:'Bolo de Chocolate'
-    }
+
+    # traz todos os objetos do banco de dados
+    receitas = Receita.objects.all()
+    
     dados = {
-        'nome_das_receitas' : receitas
+        'receitas' : receitas
     }
     return render(request, 'index.html', dados)
 
