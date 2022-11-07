6e4c124d89b3005b6188a70aac2d2310d61bccc9
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-07 10:30:49 -0300

    Added admin dashboard fields

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Receita
 # Register your models here.
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo')
+    list_display_links = ('id', 'nome_receita')
+admin.site.register(Receita, ListandoReceitas)
--- receitas/models.py
+++ receitas/models.py
@@ -10,3 +10,6 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.nome_receita
