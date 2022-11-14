ae3c9ed30f765d6ed5abacd35df77a00460593a6
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-14 20:05:12 -0300

    User login validation

--- usuarios/views.py
+++ usuarios/views.py
@@ -53,7 +53,11 @@ def login(request):
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
