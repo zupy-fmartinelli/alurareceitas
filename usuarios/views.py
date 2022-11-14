1433486581a602b6d391e0de5e962628f0db479e
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-14 19:17:43 -0300

    Added CSRF Token

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        print(nome, email, senha, senha2)
+        
+        print('Usuário criado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
     
 def login(request):
     return render(request, 'usuarios/login.html')
