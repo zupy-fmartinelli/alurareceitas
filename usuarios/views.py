bb80ffe60b21f445bcd6ab3babac1a047ebe7949
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-14 19:32:39 -0300

    User New Account

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
@@ -7,7 +8,25 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-        print(nome, email, senha, senha2)
+        
+        if not nome.strip():
+            print('o Campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        
+        if not email.strip():
+            print('o Campo email não pode ficar em branco')
+            return redirect('cadastro')
+        
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
         
         print('Usuário criado com sucesso')
         return redirect('login')
