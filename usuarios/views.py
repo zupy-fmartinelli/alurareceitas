f2a6753cc77448c483c9424da5ca6d76b41394a8
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-14 19:41:34 -0300

    Login form

--- usuarios/views.py
+++ usuarios/views.py
@@ -34,10 +34,19 @@ def cadastro(request):
         return render(request, 'usuarios/cadastro.html')
     
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print('Os campos email e senha não podem ficar em branco')
+            return redirect('login')
+        print(email, senha)
+        return redirect('dashboard')
+    
     return render(request, 'usuarios/login.html')
 
 def logout(request):
     pass
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
