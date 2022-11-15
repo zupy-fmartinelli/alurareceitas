6bb5125fdb556d8cf1157c5b38701c11d160c3a5
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-15 15:57:10 -0300

    Separacao de Responsabilidades

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,9 +5,6 @@ urlpatterns = [
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('cria/receita', views.cria_receita, name='cria_receita'),
     path('logout', views.logout, name='logout'),
-    path('deleta/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
-    path('edita/<int:receita_id>', views.edita_receita, name='edita_receita'),
-    path('atualiza/', views.atualiza_receita, name='atualiza_receita'),
+
 ]
