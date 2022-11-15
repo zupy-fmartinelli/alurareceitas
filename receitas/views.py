c3c4f7add531cc9924ae901495ce854ea57a3d94
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-15 15:49:26 -0300

    Organizing templates Folder

--- receitas/views.py
+++ receitas/views.py
@@ -14,7 +14,7 @@ def index(request):
     dados = {
         'receitas' : receitas
     }
-    return render(request, 'index.html', dados)
+    return render(request, 'receitas/index.html', dados)
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -24,7 +24,7 @@ def receita(request, receita_id):
     }
     
     
-    return render(request, 'receita.html', receita_a_exibir)
+    return render(request, 'receitas/receita.html', receita_a_exibir)
 
 def buscar(request):
     lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
@@ -38,4 +38,4 @@ def buscar(request):
         'receitas' : lista_receitas
     }            
     
-    return render(request, 'buscar.html', dados)
+    return render(request, 'receitas/buscar.html', dados)
